f0a50eead1268b95243fd9d4660537c33a973a7d
yidong.deng <yidong.deng@tenclass.com>
2022-02-11 18:01:45 +0800

    feat: modify readme and add commitlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,6 @@ module.exports = {
     indent: ["error", 4],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "single"],
-    semi: ["error", "never"],
+    semi: false,
   },
 };
--- src/controller/home-controller.ts
+++ src/controller/home-controller.ts
@@ -1,9 +1,9 @@
-import HomeService from '../service/home-service';
+import HomeService from "../service/home-service";
 
 class HomeController {
   private service: HomeService = new HomeService();
 
-  hello = async ctx => {
+  hello = async (ctx) => {
     ctx.body = await this.service.hello();
   };
 }
